32d772579f3317c0fb6ea27d6def7d46acdf363f
GuyliannEngels <guyliann.engels@umons.ac.be>
2023-10-16 13:39:50 +0200

    add import.R script

---
+++ R/import.R
@@ -0,0 +1,17 @@
+# Importation et remaniement des données
+
+
+# Etape 1 : Importation des données brutes --------------------------------
+
+
+
+# Etape 2 : Nettoyage des données  ----------------------------------------
+
+
+
+# Etape 3 : Ajout des labels et des unités --------------------------------
+
+
+# Etape 4 : Sauvegarde locale des données retravaillées -------------------
+
+
